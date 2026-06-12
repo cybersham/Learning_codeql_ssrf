87a44623363b1266cd3be7766728496e7581119a
cybersham <learningcybersham@gmail.com>
2026-06-12 19:13:00 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,10 +1,18 @@
 from flask import Flask, request
 import requests
 
+ALLOWED_DOMAINS = [
+    "example.com"
+]
+
 app = Flask(__name__)
 
 def get_target_url(user_input):
     return user_input
+
+def validate_url(url):
+    print("Validating:", url)
+    return url
 
 @app.route("/fetch")
 def fetch():
@@ -13,7 +21,12 @@ def fetch():
 
     target_url = get_target_url(url)
 
-    response = requests.get(target_url)
+    validated_url = validate_url(target_url)
+
+    if validated_url is None:
+        return "Rejected"
+
+    response = requests.get(validated_url)
 
     return response.text
 
