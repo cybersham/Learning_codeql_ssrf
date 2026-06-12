87e378263040a4494ebe5dbecc84635bcfed4b53
cybersham <learningcybersham@gmail.com>
2026-06-12 19:06:57 +0530

    Refactor fetch function to use get_target_url

--- code.py
+++ code.py
@@ -3,12 +3,17 @@ import requests
 
 app = Flask(__name__)
 
+def get_target_url(user_input):
+    return user_input
+
 @app.route("/fetch")
 def fetch():
 
     url = request.args.get("url")
 
-    response = requests.get(url)
+    target_url = get_target_url(url)
+
+    response = requests.get(target_url)
 
     return response.text
 
